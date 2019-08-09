0634d28686eb82678cccf24eb3f4be6b36208f2c
Dekel Barzilay <dekel@lusha.co>
2019-08-09 16:00:49 +0300

    Added more info to the dashboard on each queue

--- src/server/views/dashboard/queueList.js
+++ src/server/views/dashboard/queueList.js
@@ -5,6 +5,12 @@ async function handler(req, res) {
   const now = Date.now();
   const yesterday = now - (24 * 60 * 60 * 1000);
   const hourAgo = now - (60 * 60 * 1000);
+  let active = 0;
+  let waiting = 0;
+  let completedLastHourCount = 0;
+  let completedLastDayCount = 0;
+  let failedLastHourCount = 0;
+  let failedLastDayCount = 0;
   let completed, failed;
 
   for (const q of queues) {
@@ -12,7 +18,11 @@ async function handler(req, res) {
     if (!queue) return res.status(404).send({ error: 'queue not found' });
 
     q.activeCount = await queue.getActiveCount();
+    active += q.activeCount;
+
     q.waitingCount = await queue.getWaitingCount();
+    waiting += q.waitingCount;
+
     q.completedLastHourCount = 0;
     q.completedLastDayCount = 0;
     q.failedLastHourCount = 0;
@@ -24,23 +34,29 @@ async function handler(req, res) {
     for (const job of completed) {
       if (job.finishedOn > yesterday) {
         q.completedLastDayCount++;
+        completedLastDayCount++;
 
-        if (job.finishedOn > hourAgo)
+        if (job.finishedOn > hourAgo) {
           q.completedLastHourCount++;
+          completedLastHourCount++;
+        }
       }
     }
 
     for (const job of failed) {
       if (job.finishedOn > yesterday) {
         q.failedLastDayCount++;
+        failedLastDayCount++;
 
-        if (job.finishedOn > hourAgo)
+        if (job.finishedOn > hourAgo) {
           q.failedLastHourCount++;
+          failedLastHourCount++;
+        }
       }
     }
   }
 
-  return res.render('dashboard/templates/queueList', { basePath, queues });
+  return res.render('dashboard/templates/queueList', { basePath, queues, active, waiting, completedLastHourCount, completedLastDayCount, failedLastHourCount, failedLastDayCount });
 }
 
 module.exports = handler;
