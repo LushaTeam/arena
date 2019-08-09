309b0c5df14306aca4aa7fa2d517ef842af8ff58
Dekel Barzilay <dekel@lusha.co>
2019-08-09 14:38:27 +0300

    Added more info to the dashboard on each queue

--- src/server/views/dashboard/queueList.js
+++ src/server/views/dashboard/queueList.js
@@ -1,7 +1,44 @@
-function handler(req, res) {
+async function handler(req, res) {
   const {Queues} = req.app.locals;
   const queues = Queues.list();
   const basePath = req.baseUrl;
+  const now = Date.now();
+  const yesterday = now - (24 * 60 * 60 * 1000);
+  const hourAgo = now - (60 * 60 * 1000);
+  let completed, failed;
+
+  for (const q of queues) {
+    const queue = await Queues.get(q.name, q.hostId);
+    if (!queue) return res.status(404).send({ error: 'queue not found' });
+
+    q.activeCount = await queue.getActiveCount();
+    q.waitingCount = await queue.getWaitingCount();
+    q.completedLastHourCount = 0;
+    q.completedLastDayCount = 0;
+    q.failedLastHourCount = 0;
+    q.failedLastDayCount = 0;
+
+    completed = await queue.getCompleted();
+    failed = await queue.getFailed();
+
+    for (const job of completed) {
+      if (job.finishedOn > yesterday) {
+        q.completedLastDayCount++;
+
+        if (job.finishedOn > hourAgo)
+          q.completedLastHourCount++;
+      }
+    }
+
+    for (const job of failed) {
+      if (job.finishedOn > yesterday) {
+        q.failedLastDayCount++;
+
+        if (job.finishedOn > hourAgo)
+          q.failedLastHourCount++;
+      }
+    }
+  }
 
   return res.render('dashboard/templates/queueList', { basePath, queues });
 }
